1b51607a8f80cf218970e8ee683a92ef16fd1c40
Sraddheya <ggsraddheya@gmail.com>
2023-02-13 18:52:35 +0000

    implemented qrScanner

--- app/src/main/java/com/ecclesiav2/OrganisationFragment.java
+++ app/src/main/java/com/ecclesiav2/OrganisationFragment.java
@@ -12,6 +12,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import java.util.ArrayList;
 
@@ -26,7 +27,18 @@ public class OrganisationFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return  inflater.inflate(R.layout.fragment_organisation, container, false);
+        View view = inflater.inflate(R.layout.fragment_organisation, container, false);
+
+        Button qrButton = view.findViewById(R.id.qrButton);
+        qrButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), OrganisationScanner.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
 
     @Override
@@ -46,6 +58,7 @@ public class OrganisationFragment extends Fragment {
         orgRecView.setAdapter(adapter);
     }
 
+    //OPEN ACTIVITY ONCLICK
     private void setOnClickListener() {
         listener = new OrganisationAdapter.RecyclerViewClickListener() {
             @Override
---
+++ app/src/main/java/com/ecclesiav2/OrganisationScanner.java
@@ -0,0 +1,85 @@
+package com.ecclesiav2;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+import com.google.zxing.Result;
+
+public class OrganisationScanner extends AppCompatActivity {
+    private CodeScanner mCodeScanner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_organisation_scanner);
+
+        setupPermissions();
+        scanCode();
+    }
+
+    private void scanCode() {
+        CodeScannerView scannerView = findViewById(R.id.scanner_view);
+        mCodeScanner = new CodeScanner(this, scannerView);
+        mCodeScanner.setDecodeCallback(new DecodeCallback() {
+            @Override
+            public void onDecoded(@NonNull final Result result) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(OrganisationScanner.this, result.getText(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+        scannerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCodeScanner.startPreview();
+            }
+        });
+    }
+
+    private void setupPermissions() {
+        int permission = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+
+        if (permission != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.CAMERA}, 101);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 101:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "You need the camera permission to be enabled to use this feature", Toast.LENGTH_SHORT).show();
+                } else {
+                    // success
+                }
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mCodeScanner.startPreview();
+    }
+
+    @Override
+    protected void onPause() {
+        mCodeScanner.releaseResources();
+        super.onPause();
+    }
+}
