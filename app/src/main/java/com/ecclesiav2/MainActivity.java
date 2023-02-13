096d15c7a2909d2bf79744d7339f8d11e89018f4
Sraddheya <ggsraddheya@gmail.com>
2023-02-13 11:22:31 +0000

    added bottom nav bar

--- app/src/main/java/com/ecclesiav2/MainActivity.java
+++ app/src/main/java/com/ecclesiav2/MainActivity.java
@@ -1,14 +1,41 @@
 package com.ecclesiav2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    OrganisationFragment organisationFragment = new OrganisationFragment();
+    ElectionFragment electionFragment = new ElectionFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //BOTTOM NAV BAR-----------------------------------------
+        bottomNavigationView = findViewById(R.id.bottom_nav);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, organisationFragment).commit();
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.organisations:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, organisationFragment).commit();
+                        return true;
+                    case R.id.elections:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, electionFragment).commit();
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 }
