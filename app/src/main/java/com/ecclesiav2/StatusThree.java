3128c5fd8ff5f28b078be7984cf9c9b1c93787b7
Sraddheya <ggsraddheya@gmail.com>
2023-03-08 12:46:54 +0000

    status3 link working

--- app/src/main/java/com/ecclesiav2/StatusThree.java
+++ app/src/main/java/com/ecclesiav2/StatusThree.java
@@ -1,5 +1,6 @@
 package com.ecclesiav2;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -10,11 +11,22 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class StatusThree extends Fragment {
+    TextView toHelpTxt;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v =  inflater.inflate(R.layout.fragment_status_three, container, false);
+
+        toHelpTxt = v.findViewById(R.id.learnLink);
+        toHelpTxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), HelpActivity.class);
+                startActivity(intent);
+            }
+        });
+
         return v;
     }
 }
